d2c45e72ec34e979d93199e6d5d811cf47729fd6
Douglas Rodriguez <dougdeveloper85@gmail.com>
2020-03-27 02:12:45 -0600

    Enabling synopsis section and slider image preview

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Icon from './Icon';
+
+const Button = ({ children, type, icon, onClick }) => (
+  <button type={type} onClick={onClick}>
+    {icon && <Icon type={icon} />}
+    {children}
+  </button>
+)
+
+Button.defaultProps = {
+  type: 'button',
+  icon: null,
+  onClick: null
+}
+
+export default Button;
