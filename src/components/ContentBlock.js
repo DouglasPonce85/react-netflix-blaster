d5ec4c742c210047b6d229dec1404ca4623ba580
Douglas Rodriguez <dougdeveloper85@gmail.com>
2020-03-28 15:48:44 -0600

    Fixing contentBlock useCallback

--- src/components/ContentBlock.js
+++ src/components/ContentBlock.js
@@ -23,11 +23,11 @@ const ContentBlock = ({ img }) => {
             onMouseEnter={handleHover}
             onMouseLeave={handleHover}
         >
-            { img === hovered && (
+            {img === hovered && (
                 <div className="content">
                     <Icon type="play" />
                     <Icon type="info-circle" />
-                </div> )
+                </div>)
             }
 
             <img src={img} />
@@ -41,6 +41,23 @@ const BlockContainer = styled.div`
     flex-shrink: 0;
     height: 9.5vw;
     margin-right: 4px;
+    transition: 300ms, transform 250ms ease;
+    -moz-transition: 300ms, transform 250ms ease;
+    -webkit-transition: 300ms, transform 250ms ease;
+
+    &:hover {
+        -ms-transform: scale(1.5); /* IE 9 */
+        -webkit-transform: scale(1.5); /* Safari */
+        transform: scale(1.5);
+        margin-left: 60px;
+        margin-right: 65px;
+    }
+
+    &:active {
+        -ms-transform: scale(0.99); /* IE 9 */
+        -webkit-transform: scale(0.99); /* Safari */
+        transform: scale(0.99);
+    }
 
     .content {
         position: absolute;
