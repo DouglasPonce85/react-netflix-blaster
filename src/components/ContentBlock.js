ad0b8245c90641990258e4b3531c8077052ad420
Douglas Rodriguez <dougdeveloper85@gmail.com>
2020-03-27 23:52:20 -0600

    Adding ContentRow to ContentBlock

---
+++ src/components/ContentBlock.js
@@ -0,0 +1,59 @@
+/** @jsx jsx */
+import React from 'react';
+import { css, jsx } from '@emotion/core';
+
+/**
+ * @function ContentBlock
+ */
+const ContentBlock = ({ img }) => {
+    return (
+        <div css={contentStyle}>
+            <img src={img} />
+        </div>
+    )
+};
+
+const contentStyle = css`
+    position: relative;
+    flex: calc(18vw - 4px);
+    flex-shrink: 0;
+    height: 9.5vw;
+    margin-right: 4px;
+
+    .content {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        justify-content: center;
+        transition: background-color ease 0.2s;
+
+        &:hover {
+            background-color: rgba(0, 0, 0, 0.65);
+            cursor: pointer;
+        }
+
+        .Icon {
+            font-size: 32px;
+        }
+
+        .Icon:first-of-type {
+            color: red;
+            margin-right: 25px;
+        }
+
+        .Icon:last-of-type {
+            color: white;
+        }
+    }
+
+    img {
+        height: 100%;
+        width: 100%;
+        pointer-events: none;
+
+    }
+`;
+
+export default ContentBlock;
--- src/components/ContentRow.js
+++ src/components/ContentRow.js
@@ -3,26 +3,40 @@ import React from 'react';
 import { css, jsx } from '@emotion/core';
 
 import { imgs } from '../utils/global';
+import ContentBlock from './ContentBlock';
 
 /**
  * @function ContentRow
  */
-
 const ContentRow = ({ category }) => {
     return (
         <div className="ContentRow"
             css={css`
-                padding: 60px;
+                padding: 30px 40px 5px;
                 overflow-x: hidden;
             `}>
 
-            <h2>{ category }</h2>
-            <div
-                css={css`
-                    display:flex
-                `}
-            >
-                { imgs.map(img => <img key={img} src={img} />) }
+            <div css={css`
+                h2 {
+                    margin: 20px 0 10px;
+                    color: white;
+                }
+
+                .block-wrapper {
+                    display: flex;
+                    width: 100%;
+                    position: relative;
+                }
+            `}>
+                <h2>{category}</h2>
+
+                <div className="block-wrapper">
+                    {
+                        imgs.map(img => (
+                            <ContentBlock key={img} img={img} />
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
